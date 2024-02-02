4455914fe1d0d93ed04e1ad38ede311158f30e4e
Anton Stihl <anton.stihl@nordnet.se>
2024-02-02 19:47:48 +0100

    fix: too tight header, missing webkit prefix for blur prop

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const thumbnail = document.querySelector(".thumbnail-hide");
 const pageTitle = document.querySelector(".page_title");
 const pic = document.getElementById("portrait");
+const headerMonth = document.querySelector(".header_month");
 
 const imgObserver = new IntersectionObserver(
   (entries) => {
@@ -8,10 +9,12 @@ const imgObserver = new IntersectionObserver(
       if (!entry.isIntersecting) {
         thumbnail.classList.add("thumbnail-show");
         pageTitle.classList.add("page_title--padding");
+        headerMonth.classList.add("header_month-minimize");
       }
       if (entry.isIntersecting) {
         thumbnail.classList.remove("thumbnail-show");
         pageTitle.classList.remove("page_title--padding");
+        headerMonth.classList.remove("header_month-minimize");
       }
     });
   },
