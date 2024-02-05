bb17fd8f643dcead57b81cd57548236f9dec961e
Anton Stihl <anton.stihl@nordnet.se>
2024-02-05 12:33:33 +0100

    rm month in header

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const thumbnail = document.querySelector(".thumbnail-hide");
 const pageTitle = document.querySelector(".page_title");
 const pic = document.getElementById("portrait");
-const headerMonth = document.querySelector(".header_month");
 
 const imgObserver = new IntersectionObserver(
   (entries) => {
@@ -9,12 +8,10 @@ const imgObserver = new IntersectionObserver(
       if (!entry.isIntersecting) {
         thumbnail.classList.add("thumbnail-show");
         pageTitle.classList.add("page_title--padding");
-        headerMonth.classList.add("header_month-minimize");
       }
       if (entry.isIntersecting) {
         thumbnail.classList.remove("thumbnail-show");
         pageTitle.classList.remove("page_title--padding");
-        headerMonth.classList.remove("header_month-minimize");
       }
     });
   },
