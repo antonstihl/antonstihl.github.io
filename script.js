a6ee42ef4f091574dda1b15bcd7987cb474b9847
Anton Stihl <anton.stihl@nordnet.se>
2024-02-05 21:55:42 +0100

    design: rearrange header

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const thumbnail = document.querySelector(".thumbnail-hide");
 const pageTitle = document.querySelector(".page_title");
+const header = document.querySelector(".header");
 const pic = document.getElementById("portrait");
 
 const imgObserver = new IntersectionObserver(
@@ -7,11 +8,9 @@ const imgObserver = new IntersectionObserver(
     entries.forEach((entry) => {
       if (!entry.isIntersecting) {
         thumbnail.classList.add("thumbnail-show");
-        pageTitle.classList.add("page_title--padding");
       }
       if (entry.isIntersecting) {
         thumbnail.classList.remove("thumbnail-show");
-        pageTitle.classList.remove("page_title--padding");
       }
     });
   },
