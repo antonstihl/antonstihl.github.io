f8e00c9301c0f75abd09ff4badd86087e3f103e7
Anton Stihl <anton.stihl@nordnet.se>
2024-03-24 21:14:07 +0100

    refactor: less js, better css

--- script.js
+++ script.js
@@ -19,18 +19,5 @@ document
 document.querySelectorAll(".card").forEach((e) =>
   e.addEventListener("click", () => {
     e.classList.toggle("card--expand");
-    e.querySelector(".accordion-button")?.classList?.toggle(
-      "accordion-button--open"
-    );
-
-    if (e.id === "ikea") {
-      e.classList.toggle("ikea-theme");
-    } else if (e.id === "nordnet") {
-      e.classList.toggle("nordnet-theme");
-    } else if (e.id === "evry") {
-      e.classList.toggle("evry-theme");
-    } else if (e.id === "chs") {
-      e.classList.toggle("chs-theme");
-    }
   })
 );
