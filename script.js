d3dcb9005386b931ecbb3108048f5c2d389d9f12
Anton Stihl <anton.stihl@nordnet.se>
2025-05-01 17:00:39 +0200

    simplification and improved semantics + a11y

--- script.js
+++ script.js
@@ -3,21 +3,3 @@ document
   .addEventListener("click", (e) =>
     localStorage.setItem("theme", e.target.checked ? "dark" : "light")
   );
-
-const revealObserver = new IntersectionObserver((entries) =>
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("reveal");
-    }
-  })
-);
-
-document
-  .querySelectorAll(".tech-grid li")
-  .forEach((e) => revealObserver.observe(e));
-
-document.querySelectorAll(".card").forEach((e) =>
-  e.addEventListener("click", () => {
-    e.classList.toggle("card--expand");
-  })
-);
